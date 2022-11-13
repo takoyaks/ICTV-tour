412ded5b0c4043da7aeb45611cec8dff4627b36f
takoyaks <97886418+takoyaks@users.noreply.github.com>
2022-11-13 11:46:38 +0800

    Add files via upload

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA96ilb-pZZHtfI9xo6uUjnScNhUP9zcx8",
+    authDomain: "ictv-tour-admin-test.firebaseapp.com",
+    databaseURL: "https://ictv-tour-admin-test-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ictv-tour-admin-test",
+    storageBucket: "ictv-tour-admin-test.appspot.com",
+    messagingSenderId: "320386490527",
+    appId: "1:320386490527:web:4304fb962b4dad85edc2aa"
+};
+firebase.initializeApp(firebaseConfig);
